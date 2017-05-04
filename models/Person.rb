636b44cb5c2a2b70f01ca3a74a07f7eab0186ede
Richard Edwards <richardedwards899@gmail.com>
2017-05-04 19:26:17 +0100

    add Person.find method

--- models/Person.rb
+++ models/Person.rb
@@ -24,24 +24,24 @@ class Person
     return nil
   end
 
-  # def delete()
-  #   sql = "DELETE FROM customers where id = #{@id};"
-  #   SqlRunner.run(sql)
-  # end
+  def delete()
+    sql = "DELETE FROM people where id = #{@id};"
+    SqlRunner.run(sql)
+  end
 
-  # def Customer.all()
-  #   sql = "SELECT * FROM customers;"
-  #   customer_hashes = SqlRunner.run(sql)
-  #   return customer_hashes.map { |customer_hash| Customer.new(customer_hash) }
-  # end
+  def Person.all()
+    sql = "SELECT * FROM people;"
+    people_hashes = SqlRunner.run(sql)
+    return people_hashes.map { |person_hash| Person.new(person_hash) }
+  end
 
   # def Customer.delete_all()
   #   sql = "DELETE FROM customers;"
   #   SqlRunner.run(sql)
   # end
 
-  # def Customer.find(id)
-  #   sql = "SELECT * FROM customers WHERE id = #{id};"
-  #   return Customer.new ( SqlRunner.run(sql)[0] )
-  # end
+  def Person.find(id)
+    sql = "SELECT * FROM people WHERE id = #{id};"
+    return Person.new ( SqlRunner.run(sql)[0] )
+  end
 end
