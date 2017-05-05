0c369655d783605ede6c732032066f3fafc78c6e
Richard Edwards <richardedwards899@gmail.com>
2017-05-05 07:55:49 +0100

    add test for create Transaction

--- specs/transaction_spec.rb
+++ specs/transaction_spec.rb
@@ -1,18 +1,33 @@
 require('minitest/autorun')
 require('minitest/emoji')
 require_relative('../models/Transaction.rb')
+require_relative('../models/Tag.rb')
+require_relative('../models/Person.rb')
 
 class TestTransactionClass < MiniTest::Test
 
   def test_can_create_transaction
+
+    person = Person.new({
+      "first_name" => "Reggie",
+      "last_name" => "Buttons",
+      "budget" => 1050
+      })
+
+    tag = Tag.new({
+      "tag_name" => "groceries"
+      })
+
     transaction = Transaction.new( {
-    #   "first_name" => "Richard", 
-    #   "last_name" => "Edwards", 
-    #   "budget" => 1000
-    #   })
-    # assert_equal("Richard", person.first_name)
-    # assert_equal("Edwards", person.last_name)
-    # assert_equal(1000, person.budget)
+      "person_id" => person.id,
+      "tag_id" => tag.id,
+      "merchant" => "Tesco",
+      "value" => "10",
+      "purchased_on" => "17/2/2017"
+      })
+    assert_equal("Tesco", transaction.merchant)
+    assert_equal(10, transaction.value)
+    assert_equal("17/2/2017", transaction.purchased_on)
   end
 
 end
