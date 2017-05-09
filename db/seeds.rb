701c3be88658ea1d472aeb566dd381c9b4c05437
Richard Edwards <richardedwards899@gmail.com>
2017-05-09 19:44:05 +0100

    fix display of decimals

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,16 @@ Person.delete_all
 person1 = Person.new({
   "first_name" => "Richard",
   "last_name" => "Edwards",
-  "budget" => 10_000.00
+  "budget" => 5_000.00
   })
 
 person1.save
 
-tag1 = Tag.new({ "tag_name" => "groceries" })
-tag2 = Tag.new({ "tag_name" => "motoring" })
-tag3 = Tag.new({ "tag_name" => "clothes" })
-tag4 = Tag.new({ "tag_name" => "entertainment" })
-tag5 = Tag.new({ "tag_name" => "holidays" })
+tag1 = Tag.new({ "tag_name" => "Groceries" })
+tag2 = Tag.new({ "tag_name" => "Motoring" })
+tag3 = Tag.new({ "tag_name" => "Clothes" })
+tag4 = Tag.new({ "tag_name" => "Entertainment" })
+tag5 = Tag.new({ "tag_name" => "Holidays" })
 
 tag1.save
 tag2.save
@@ -65,13 +65,52 @@ transaction5 = Transaction.new({
   "value" => "175.99",
   "purchased_on" => "1/2/2017"
   })
+transaction6 = Transaction.new({
+  "person_id" => person1.id,
+  "tag_id" => tag1.id,
+  "merchant" => "Sainsburys",
+  "value" => "65.73",
+  "purchased_on" => "17/1/2017"
+  })
+transaction7 = Transaction.new({
+  "person_id" => person1.id,
+  "tag_id" => tag2.id,
+  "merchant" => "Ian Murray Motorcycles",
+  "value" => "250.00",
+  "purchased_on" => "20/4/2017"
+  })
+transaction8 = Transaction.new({
+  "person_id" => person1.id,
+  "tag_id" => tag3.id,
+  "merchant" => "New Look",
+  "value" => "40.00",
+  "purchased_on" => "11/2/2017"
+  })
+transaction9 = Transaction.new({
+  "person_id" => person1.id,
+  "tag_id" => tag4.id,
+  "merchant" => "The Slug and Lettuce",
+  "value" => "13.20",
+  "purchased_on" => "10/5/2017"
+  })
+transaction10 = Transaction.new({
+  "person_id" => person1.id,
+  "tag_id" => tag5.id,
+  "merchant" => "Aeropuerto de Malaga",
+  "value" => "99.99",
+  "purchased_on" => "1/2/2017"
+  })
 
 transaction1.save
 transaction2.save
 transaction3.save
 transaction4.save
 transaction5.save
-
+transaction6.save
+transaction7.save
+transaction8.save
+transaction9.save
+transaction10.save
 
 binding.pry
 nil
